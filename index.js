6918ebc8d512af95491bf7a8caaec3054e5e26c1
Naveen das <naveenkumar99444e4@gmail.com>
2026-09-15 09:13:32 +0530

    Improve time display logic in index.js
    Refactor time formatting and fix hour, minute, second checks.

--- index.js
+++ index.js
@@ -1,50 +1,43 @@
 console.log("Digital Time");
-
 let timeObj = document.getElementById("timer");
 
-// Creating function
+// creating modern function
 const startTime = () => {
-
     let date = new Date();
-
     let hour = date.getHours();
     let min = date.getMinutes();
-    let sec = date.getSeconds();
+  let sec = date.getSeconds();
+  
 
-    // Convert 24-hour format into 12-hour format
-    if (hour === 0) {
-        hour = 12;
-    } else if (hour > 12) {
-        hour = hour % 12;
+     // convert 24 hour into 12 hour time
+    if(hour > 12){
+        hour = hour%12;
     }
 
-
-    // Check hour
-    if (hour < 10) {
-        hour = "0" + hour;
+    // check hour
+    if(hour > 9){
+    }
+    else{
+        hour = "0"+hour;
     }
 
-
-    // Check minute
-    if (min < 10) {
-        min = "0" + min;
+    // check min
+     if(min > 9){
+    }
+    else{
+        min = "0"+min;
+    }
+    
+    //check sec
+    if(sec > 9){
+    }
+    else{
+        sec = "0"+sec;
     }
 
+    return `${hour} : ${min} : ${sec} ${ampm}`;
+}
 
-    // Check second
-    if (sec < 10) {
-        sec = "0" + sec;
-    }
-
-
-    // Return final time
-    return `${hour} : ${min} : ${sec}`;
-};
-
-
-// Update clock every second
 setInterval(function setTime() {
-
-    timeObj.innerHTML = startTime();
-
-}, 1000);
+  timeObj.innerHTML = startTime();
+},1000);
