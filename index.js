f471d5690d2ea70ebec988db4d63da0c4b462860
Naveen das <naveenkumar99444e4@gmail.com>
2026-09-15 09:00:52 +0530

    Refactor time formatting logic in startTime function

--- index.js
+++ index.js
@@ -1,52 +1,61 @@
 console.log("Digital Time");
+
 let timeObj = document.getElementById("timer");
 
-// creating modern function
+// Creating function
 const startTime = () => {
+
     let date = new Date();
+
     let hour = date.getHours();
     let min = date.getMinutes();
-  let sec = date.getSeconds();
-    let ampm; 
-    
-    if(hour >= 12 && hour < 24){
+    let sec = date.getSeconds();
+
+    let ampm;
+
+
+    // Check AM or PM
+    if (hour >= 12) {
+        ampm = "PM";
+    } else {
         ampm = "AM";
     }
 
-    if(hour < 12){
-          ampm = "PM";
-    }
-    
 
-     // convert hour into normal time
-    if(hour > 12){
-        hour = hour%12;
+    // Convert 24-hour format into 12-hour format
+    if (hour === 0) {
+        hour = 12;
+    } else if (hour > 12) {
+        hour = hour % 12;
     }
 
-    // check hour
-    if(hour > 9){
-    }
-    else{
-        hour = "0"+hour;
+
+    // Check hour
+    if (hour < 10) {
+        hour = "0" + hour;
     }
 
-    // check min
-     if(min > 9){
-    }
-    else{
-        min = "0"+min;
-    }
-    
-    //check sec
-    if(sec > 9){
-    }
-    else{
-        sec = "0"+sec;
+
+    // Check minute
+    if (min < 10) {
+        min = "0" + min;
     }
 
+
+    // Check second
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+
+
+    // Return final time
     return `${hour} : ${min} : ${sec} ${ampm}`;
-}
+};
 
+
+// Update clock every second
 setInterval(function setTime() {
-  timeObj.innerHTML = startTime();
-},1000);
+
+    timeObj.innerHTML = startTime();
+
+}, 1000);
